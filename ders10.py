fdf301b85e35008907b970aec88d717e8e87f782
EmirAKSOY1 <72320002+EmirAKSOY1@users.noreply.github.com>
2023-03-30 12:59:06 +0300

    Update ders10.py

--- ders10.py
+++ ders10.py
@@ -3,13 +3,17 @@
 import cv2 
 cap =cv2.VideoCapture(0)#Kendi WebCam için
 
-
+yol=0
 while True:
     ret,frame=cap.read()  #ret başarılı mı değil mi olduğunu tutar frame video için yakaladığı her fotoğraf karesidir
     cv2.imshow("video",frame)#yakalanan her kareyi ekranda göster
-    a= cv2.waitKey(500) #videonun hızını belirler ne kdar yüksek olursa fps o kadar düşük olur
+    a= cv2.waitKey(1) #videonun hızını belirler ne kdar yüksek olursa fps o kadar düşük olur
+
     if a ==27 :
-        break
+        path=f"resim/video{yol}.jpg"
+        cv2.imwrite(path,frame)
+        yol+=1
+    
 
 
 cap.release() #yakalanan görüntüleri bırakır
